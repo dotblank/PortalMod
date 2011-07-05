43c95f67cc204b8bbede3b682c6076fbc1813b4e
Elias Woods <EliasWoods@gmail.com>
2011-07-05 16:44:49 -0400

    Reduce code and redesign portal format

--- src/PPPlayerListener/PortalPlayerListener.java
+++ src/PPPlayerListener/PortalPlayerListener.java
@@ -4,9 +4,9 @@ import org.bukkit.Location;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.World;
-import us.discovr.portalplugin.PortalPlugin;
+
+import com.precipicegames.portalplugin.PortalPlugin;
 
 public class PortalPlayerListener extends PlayerListener {
     private final PortalPlugin plugin;
@@ -56,6 +56,16 @@ public class PortalPlayerListener extends PlayerListener {
 			}
 		}
 		return 0;
+	}
+	
+	private int readColumn(int x,int z, int y, int depth, Mode m, World w)
+	{
+		int[] codes = new int[depth];
+		for(int i = 0; i < depth; i++)
+		{
+			codes[i] = getEncodeVal(m,x,z,y-i,w);
+		}
+		return convertbase(m,codes,depth);
 	}
 	
     private int find2dir(Mode m,int x, int z , int y, World w) {
@@ -119,8 +129,6 @@ public class PortalPlayerListener extends PlayerListener {
 	    int orientationlevel = (int) to.getY()-1;
 	    //int oX = modX;
 	    //int oZ = modZ;
-	    int scode1 = 1;
-	    int scode2 = 1;
 	    //Change elevation for nether portal
 	    int readdirection = -1; //0 is for +x 1 is +z 2 is -x 3 is -z
 	    if(w.getBlockTypeIdAt(modX, (int)to.getY(), modZ)==90)
@@ -159,127 +167,58 @@ public class PortalPlayerListener extends PlayerListener {
 	    else
 	    {
 		float rotation = 0;
-		int options = 0;
-		boolean yset =false;
-		int[] code1 = new int[5];
-		int[] code2 = new int[5];
-		int[] codeY = new int[5];
 		int xcodenum=0;
+		int goto1 = 0;
+		int goto2 = 0;
+		int gotoY = 126;
 		switch (readdirection){
 		case -1:
 		    return; 
 		case 0:
-		    code1[0] = getEncodeVal(mode,modX+1,modZ,orientationlevel,w);
-		    code1[1] = getEncodeVal(mode,modX+1,modZ,orientationlevel-1,w);
-		    code1[2] = getEncodeVal(mode,modX+1,modZ,orientationlevel-2,w);
-		    code1[3] = getEncodeVal(mode,modX+1,modZ,orientationlevel-3,w);
-
-		    rotation = getEncodeVal(mode,modX+1,modZ,orientationlevel-4,w)*45;
-		    scode1 = -1;
+			goto1 = -readColumn(modX+1,modZ, orientationlevel-1, 4, mode, w);
+			rotation = readColumn(modX-1,modZ, orientationlevel-4, 1, mode, w)*45;
 		    xcodenum = 1;
-		    code2[0] = getEncodeVal(mode,modX,modZ,orientationlevel-1,w);
-		    code2[1] = getEncodeVal(mode,modX,modZ,orientationlevel-2,w);
-		    code2[2] = getEncodeVal(mode,modX,modZ,orientationlevel-3,w);
-		    code2[3] = getEncodeVal(mode,modX,modZ,orientationlevel-4,w);
 		    if(find2dir(mode,modX, modZ, orientationlevel-3,w) != -1)
-			scode2 = -1;
+		    goto2 = -readColumn(modX,modZ, orientationlevel-1, 4, mode, w);
 		    else
-			scode2 = 1;
-
-		    if(w.getBlockTypeIdAt(modX-1, orientationlevel-1, modZ)!=0)
-		    {
-			codeY[0] = getEncodeVal(mode,modX-1,modZ,orientationlevel-1,w);
-			codeY[1] = getEncodeVal(mode,modX-1,modZ,orientationlevel-2,w);
-			codeY[2] = getEncodeVal(mode,modX-1,modZ,orientationlevel-3,w);
-			options = getEncodeVal(mode,modX-1,modZ,orientationlevel-4,w);
-			yset =true;
-		    }
+			goto2 = readColumn(modX,modZ, orientationlevel-1, 4, mode, w);
+		    
+		    gotoY = 255 - readColumn(modX-1,modZ, orientationlevel-1, 2, mode, w);
 		    break;
 		case 1:
-		    code1[0] = getEncodeVal(mode,modX,modZ+1,orientationlevel,w);
-		    code1[1] = getEncodeVal(mode,modX,modZ+1,orientationlevel-1,w);
-		    code1[2] = getEncodeVal(mode,modX,modZ+1,orientationlevel-2,w);
-		    code1[3] = getEncodeVal(mode,modX,modZ+1,orientationlevel-3,w);
-
-		    rotation = getEncodeVal(mode,modX,modZ+1,orientationlevel-4,w)*45;
-		    scode1 = -1;
+			goto1 = -readColumn(modX,modZ+1, orientationlevel-1, 4, mode, w);
+			rotation = readColumn(modX,modZ-1, orientationlevel-4, 1, mode, w)*45;
 		    xcodenum = 2;
-		    code2[0] = getEncodeVal(mode,modX,modZ,orientationlevel-1,w);
-		    code2[1] = getEncodeVal(mode,modX,modZ,orientationlevel-2,w);
-		    code2[2] = getEncodeVal(mode,modX,modZ,orientationlevel-3,w);
-		    code2[3] = getEncodeVal(mode,modX,modZ,orientationlevel-4,w);
 		    if(find2dir(mode,modX, modZ, orientationlevel-3,w) != -1)
-			scode2 = -1;
+		    goto2 = -readColumn(modX,modZ, orientationlevel-1, 4, mode, w);
 		    else
-			scode2 = 1;
-
-		    if(w.getBlockTypeIdAt(modX, orientationlevel-1, modZ-1)!=0)
-		    {
-			codeY[0] = getEncodeVal(mode,modX,modZ-1,orientationlevel-1,w);
-			codeY[1] = getEncodeVal(mode,modX,modZ-1,orientationlevel-2,w);
-			codeY[2] = getEncodeVal(mode,modX,modZ-1,orientationlevel-3,w);
-			options = getEncodeVal(mode,modX,modZ-1,orientationlevel-4,w);
-			yset =true;
-		    }
+			goto2 = readColumn(modX,modZ, orientationlevel-1, 4, mode, w);
+		    
+		    gotoY = 255 - readColumn(modX,modZ-1, orientationlevel-1, 2, mode, w);
 		    break;
 		case 2:
-		    code1[0] = getEncodeVal(mode,modX-1,modZ,orientationlevel,w);
-		    code1[1] = getEncodeVal(mode,modX-1,modZ,orientationlevel-1,w);
-		    code1[2] = getEncodeVal(mode,modX-1,modZ,orientationlevel-2,w);
-		    code1[3] = getEncodeVal(mode,modX-1,modZ,orientationlevel-3,w);
-
-		    rotation = getEncodeVal(mode,modX-1,modZ,orientationlevel-4,w)*45;
-		    scode1 = 1;
+			goto1 = readColumn(modX-1,modZ, orientationlevel-1, 4, mode, w);
+			rotation = readColumn(modX+1,modZ, orientationlevel-4, 1, mode, w)*45;
 		    xcodenum = 1;
-		    code2[0] = getEncodeVal(mode,modX,modZ,orientationlevel-1,w);
-		    code2[1] = getEncodeVal(mode,modX,modZ,orientationlevel-2,w);
-		    code2[2] = getEncodeVal(mode,modX,modZ,orientationlevel-3,w);
-		    code2[3] = getEncodeVal(mode,modX,modZ,orientationlevel-4,w);
 		    if(find2dir(mode,modX, modZ, orientationlevel-3,w) != -1)
-			scode2 = 1;
+		    goto2 = readColumn(modX,modZ, orientationlevel-1, 4, mode, w);
 		    else
-			scode2 = -1;
-
-		    if(w.getBlockTypeIdAt(modX+1, orientationlevel-1, modZ)!=0)
-		    {
-			codeY[0] = getEncodeVal(mode,modX+1,modZ,orientationlevel-1,w);
-			codeY[1] = getEncodeVal(mode,modX+1,modZ,orientationlevel-2,w);
-			codeY[2] = getEncodeVal(mode,modX+1,modZ,orientationlevel-3,w);
-			options = getEncodeVal(mode,modX+1,modZ,orientationlevel-4,w);
-			yset =true;
-		    }
+			goto2 = -readColumn(modX,modZ, orientationlevel-1, 4, mode, w);
+		    
+		    gotoY = 255 - readColumn(modX+1,modZ, orientationlevel-1, 2, mode, w);
 		    break;
 		case 3:
-		    code1[0] = getEncodeVal(mode,modX,modZ-1,orientationlevel,w);
-		    code1[1] = getEncodeVal(mode,modX,modZ-1,orientationlevel-1,w);
-		    code1[2] = getEncodeVal(mode,modX,modZ-1,orientationlevel-2,w);
-		    code1[3] = getEncodeVal(mode,modX,modZ-1,orientationlevel-3,w);
-
-
-		    rotation = getEncodeVal(mode,modX,modZ-1,orientationlevel-4,w)*45;
-		    scode1 = 1;
+			goto1 = readColumn(modX,modZ-1, orientationlevel-1, 4, mode, w);
+			rotation = readColumn(modX,modZ+1, orientationlevel-4, 1, mode, w)*45;
 		    xcodenum = 2;
-		    code2[0] = getEncodeVal(mode,modX,modZ,orientationlevel-1,w);
-		    code2[1] = getEncodeVal(mode,modX,modZ,orientationlevel-2,w);
-		    code2[2] = getEncodeVal(mode,modX,modZ,orientationlevel-3,w);
-		    code2[3] = getEncodeVal(mode,modX,modZ,orientationlevel-4,w);
 		    if(find2dir(mode,modX, modZ, orientationlevel-3,w) != -1)
-			scode2 = 1;
+		    goto2 = readColumn(modX,modZ, orientationlevel-1, 4, mode, w);
 		    else
-			scode2 = -1;
-
-		    if(w.getBlockTypeIdAt(modX, orientationlevel-1, modZ+1)!=0)
-		    {
-			codeY[0] = getEncodeVal(mode,modX,modZ+1,orientationlevel-1,w);
-			codeY[1] = getEncodeVal(mode,modX,modZ+1,orientationlevel-2,w);
-			codeY[2] = getEncodeVal(mode,modX,modZ+1,orientationlevel-3,w);
-			options = getEncodeVal(mode,modX,modZ+1,orientationlevel-4,w);
-			yset = true;
-		    }
+			goto2 = -readColumn(modX,modZ, orientationlevel-1, 4, mode, w);
+		    
+		    gotoY = 255 - readColumn(modX,modZ+1, orientationlevel-1, 2, mode, w);
 		    break;
 		}
-		int goto1 = convertbase(mode,code1,4)*scode1;
-		int goto2 = convertbase(mode,code2,4)*scode2;
 		Location loc = new Location(w, 0,0,0);
 		if(xcodenum == 1)
 		{
@@ -295,13 +234,10 @@ public class PortalPlayerListener extends PlayerListener {
 		    loc.setZ(modZ+goto1);
 		    //loc.z = oZ+goto1;
 		}
-		int gotoY = 126;
-		if(yset)
-		    gotoY = 164-convertbase(mode,codeY,3);
 		if(gotoY <= 0)
 			gotoY = 2;
 		loc.setY(gotoY);
-		if(options == 0 || options == 2)
+		if(rotation < 360)
 		    loc.setYaw(event.getPlayer().getLocation().getYaw() + rotation);
 		else
 		    loc.setYaw(rotation);
@@ -317,8 +253,13 @@ public class PortalPlayerListener extends PlayerListener {
 		//	loc.z += 0.5f;
 		
 		int limit = plugin.getConfiguration().getInt("border-limit", 1000);
-		if(Math.abs(loc.getX()-event.getPlayer().getWorld().getSpawnLocation().getX())<=limit 
-			&& Math.abs(loc.getZ()-event.getPlayer().getWorld().getSpawnLocation().getZ())<=limit)
+		Location center = event.getPlayer().getWorld().getSpawnLocation();
+		if(plugin.getConfiguration().getBoolean("center-origin",false))
+		{
+			center = new Location(event.getPlayer().getWorld(),0,0,0);
+		}
+		if(Math.abs(loc.getX()-center.getX())<=limit 
+			&& Math.abs(loc.getZ()-center.getZ())<=limit)
 		{
 		    event.getPlayer().sendMessage(ChatColor.DARK_GRAY+"Teleporting to "+loc.getX()+" "+loc.getY()+" "+loc.getZ());
 			//event.getPlayer().teleportTo(loc);
@@ -334,7 +275,6 @@ public class PortalPlayerListener extends PlayerListener {
 
 	    }
 	}
-
 	return;
     }
 }
