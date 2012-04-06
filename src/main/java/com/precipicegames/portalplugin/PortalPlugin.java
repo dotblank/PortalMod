af423254cddd07eae9da623f671367aae5260f74
Matt Carlson (Zeryl) <saryon@swbell.net>
2012-04-06 12:44:30 -0500

    Testing changes to portal plugin

--- src/main/java/com/precipicegames/portalplugin/PortalPlugin.java
+++ src/main/java/com/precipicegames/portalplugin/PortalPlugin.java
@@ -1,6 +1,7 @@
 package com.precipicegames.portalplugin;
 
 import java.util.HashMap;
+import java.util.logging.Level;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -16,7 +17,7 @@ public class PortalPlugin extends JavaPlugin {
     public FileConfiguration config;
     private File configFile = new File(this.getDataFolder(), "config.yml");
     private final PortalPlayerListener listener = new PortalPlayerListener(this);
-    //private PPListenerv2 listener = new PPListenerv2(this); //New version
+
     public final HashMap<Player, Location> lastposition = new HashMap<Player, Location>();
     public PluginDescriptionFile pdf;
 
@@ -33,9 +34,8 @@ public class PortalPlugin extends JavaPlugin {
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(listener, this);
 
-        // EXAMPLE: Custom code, here we just output some info so we can check all is well
-        //int limit = getConfiguration().getInt("border-limit", 1000);
-        getServer().getLogger().info(pdf.getName() + " version " + pdf.getVersion() + " is enabled!");
+
+        getServer().getLogger().log(Level.INFO, "{0} version {1} is enabled!", new Object[]{pdf.getName(), pdf.getVersion()});
         //System.out.println("["+pdf.getName()+"] portal distance limit: "+limit);
         config = getConfig();
     }
